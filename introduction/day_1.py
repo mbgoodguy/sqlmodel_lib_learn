d5c894fb5f111482bc781ac29dfbb4fb1ed69323
mbgoodguy <nerooren4001@gmail.com>
2024-03-22 12:08:57 +0300

    Update day_1.py

--- introduction/day_1.py
+++ introduction/day_1.py
@@ -1,11 +1,15 @@
 import random
 import string
+from pathlib import Path
 from typing import Optional
 
 from sqlalchemy import create_engine, column, func
 from sqlmodel import SQLModel, Field, select, Session
 
-engine = create_engine("sqlite:///database.sqlite", echo=True)
+db_dir_path = Path(__file__).parent.parent / 'databases'
+sqlite_file_name = "day_1.sqlite"
+sqlite_url = f"sqlite:///{db_dir_path}/{sqlite_file_name}"
+engine = create_engine(sqlite_url, echo=True)
 session = Session(engine)
 
 
@@ -31,6 +35,17 @@ def generate_usernames(count: int, length: int) -> list:
 
 # print(generate_usernames(10, 6))
 
+def check_user_exist(db: session, user_id: int):
+    user = db.get(User, user_id)
+    if user is not None:
+        print(f'User with id {user_id} exists')
+        return user
+    return None
+
+
+def print_no_user_with(user_id: int):
+    print(f'No user with id {user_id}')
+
 
 def add_users(count: 10, length: 6):
     with session as db:
@@ -50,7 +65,7 @@ def get_users_containing(num: int):
         print(f'List of usernames containing {num}: {usernames}')
 
 
-def get_users_where():
+def get_users_w_full_uppercase():
     with session as db:
         query = select(User).where(func.upper(User.username) == User.username)
         res = session.exec(query).all()
@@ -74,26 +89,33 @@ def get_users_with_limit_and_offset(limit: int, offset: int):
 
 def update_user_by_id(user_id: int):
     with session as db:
-        user = db.get(User, user_id)
-        user.username = 'Aboba'
-        db.commit()
-        print(f'User with id {user_id} has been updated')
+        user = check_user_exist(db=db, user_id=user_id)
+        if user:
+            user.username = 'Aboba'
+            db.commit()
+            print(f'User with id {user_id} has been updated')
+        else:
+            print_no_user_with(user_id)
 
 
 def delete_user_by_id(user_id: int):
     with session as db:
-        user = db.get(User, user_id)
-        db.delete(user)
-        db.commit()
-        print(f'User with id {user_id} has been deleted')
+        user = check_user_exist(db=db, user_id=user_id)
+        if user is not None:
+            db.delete(user)
+            db.commit()
+            print(f'User with id {user_id} has been deleted')
+        else:
+            print_no_user_with(user_id)
 
 
 if __name__ == '__main__':
     # create_db_and_tables()
     # add_users(count=15, length=4)  # ok
+    # check_user_exist(db=session, user_id=1)
     # get_users_containing(2)
-    # get_users_where()
+    # get_users_w_full_uppercase()
     # get_users_with_limit(value=6)
     # get_users_with_limit_and_offset(5, 5)
     # update_user_by_id(5)
-    delete_user_by_id(2)
+    delete_user_by_id(1)
